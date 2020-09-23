e4a7f73d0ca11102c8166ce5c5cfb311d25ca1f4
Zhe Yu <zhe@Zhe-Yus-MacBook-Pro.local>
2020-09-23 08:37:51 -0400

    A4

--- assignments/assignment4/my_KNN.py
+++ assignments/assignment4/my_KNN.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from collections import Counter
 
 class my_KNN:
 
@@ -15,20 +17,52 @@ class my_KNN:
         # X: pd.DataFrame, independent variables, float
         # y: list, np.array or pd.Series, dependent variables, int or str
         self.classes_ = list(set(list(y)))
-        # write your code below
+        self.X = X
+        self.y = y
         return
+
+    def dist(self,x):
+        # Calculate distances of training data to a single input data point (np.array)
+        if self.metric == "minkowski":
+            distances = ((np.abs(self.X - x) ** self.p).sum(axis=1)) ** (1 / self.p)
+        elif self.metric == "euclidean":
+            distances = ((self.X - x)**2).sum(axis=1) ** 0.5
+        elif self.metric == "manhattan":
+            distances = np.abs(self.X - x).sum(axis=1)
+        elif self.metric == "cosine":
+            distances = 1 - (self.X * x).sum(axis=1)/(np.sum(x**2)**0.5 * (self.X**2).sum(axis=1)**0.5)
+        else:
+            raise Exception("Unknown criterion.")
+        return distances
+
+    def k_neighbors(self,x):
+        # Return the stats of the labels of k nearest neighbors to a single input data point (np.array)
+        # Output: Counter(labels of the self.n_neighbors nearest neighbors)
+        distances = self.dist(x)
+        return Counter(self.y[np.argsort(distances)[:self.n_neighbors]])
 
     def predict(self, X):
         # X: pd.DataFrame, independent variables, float
         # return predictions: list
-        # write your code below
+        probs = self.predict_proba(X)
+        predictions = [self.classes_[np.argmax(prob)] for prob in probs.to_numpy()]
         return predictions
 
     def predict_proba(self, X):
         # X: pd.DataFrame, independent variables, float
         # prob is a dict of prediction probabilities belonging to each categories
         # return probs = pd.DataFrame(list of prob, columns = self.classes_)
-        # write your code below
+
+        probs = []
+        try:
+            X_feature = X[self.X.columns]
+        except:
+            raise Exception("Input data mismatch.")
+
+        for x in X_feature.to_numpy():
+            neighbors = self.k_neighbors(x)
+            probs.append({key: neighbors[key]/float(self.n_neighbors) for key in self.classes_})
+        probs = pd.DataFrame(probs, columns=self.classes_)
         return probs
 
 
