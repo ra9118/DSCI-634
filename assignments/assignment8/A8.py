ce51ca849680ef548d35bfb188d79c833e690545
Zhe Yu <zhe@Zhe-Yus-MacBook-Pro.local>
2020-10-20 09:05:57 -0400

    A8

--- assignments/assignment8/A8.py
+++ assignments/assignment8/A8.py
@@ -1,9 +1,7 @@
 from my_evaluation import my_evaluation
 import pandas as pd
-####### Add assignment2 to sys.path to import my_DT #######
-import sys
-sys.path.insert(0,'../assignment2')
-from my_DT import my_DT
+from sklearn.tree import DecisionTreeClassifier
+
 ##################################################
 
 if __name__ == "__main__":
@@ -14,21 +12,22 @@ if __name__ == "__main__":
     X = data_train[independent]
     y = data_train["Species"]
     # Fit model
-    clf = my_DT()
+    clf = DecisionTreeClassifier(criterion = "entropy", max_depth = 2)
     clf.fit(X, y)
     # Predict on training data
     predictions = clf.predict(X)
+    print(predictions)
     # Predict probabilities
     probs = clf.predict_proba(X)
+    probs = pd.DataFrame({key: probs[:,i] for i, key in enumerate(clf.classes_)})
     # Evaluate results
     metrics = my_evaluation(predictions, y, probs)
     result = {}
     for target in clf.classes_:
         result[target] = {}
-        result[target]["prec"] = metrics.prec(target)
+        result[target]["prec"] = metrics.precision(target)
         result[target]["recall"] = metrics.recall(target)
         result[target]["f1"] = metrics.f1(target)
-        result[target]["fpr"] = metrics.fpr(target)
         result[target]["auc"] = metrics.auc(target)
     print(result)
     f1 = {average: metrics.f1(target=None, average=average) for average in ["macro", "micro", "weighted"]}
